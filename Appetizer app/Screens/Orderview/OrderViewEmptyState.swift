5a2013be444f2ef490f01018860531a1ca06df6f
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-09-19 00:11:31 +0530

    Create OrderEmptyView to display when no items are in order

---
+++ Appetizer app/Screens/Orderview/OrderViewEmptyState.swift
@@ -0,0 +1,18 @@
+//
+//  OrderViewEmptyState.swift
+//  Appetizer app
+//
+//  Created by Shashank Yadav on 19/09/25.
+//
+
+import SwiftUI
+
+struct OrderViewEmptyState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderViewEmptyState()
+}
