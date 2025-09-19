1893f8977561cbc126f2d039d7b640e5a18cb71c
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-09-19 19:59:13 +0530

    rename

--- Appetizer app/Screens/AccountView/AccountView.swift
+++ Appetizer app/Screens/AccountView/AccountView.swift
@@ -9,18 +9,51 @@ import SwiftUI
 
 struct AccountView: View {
     @StateObject private var accountViewModel = AccountViewModel()
+    @FocusState private var focusedTextField: FormTextField? // it keeps the track of which is focused
+                                                            
+    
+    enum FormTextField {
+        case firstName
+        case lastName
+        case email
+        
+        
+    
+    }
 
     
     var body: some View {
         NavigationView{
             Form { // as we are the using the form it automatically apply the styling that the form will need
                 Section { // section is used to group the related                                           contents together
+                    
                     TextField("First Name:", text: $accountViewModel.user.firstName)
+                        .focused($focusedTextField, equals:.firstName)
+                        .onSubmit{
+                            focusedTextField = .lastName
+                        }
+                        .submitLabel(.next)
+                    
+                    // in this we need to provide the binding which binds the focus of the state adn equals to here is for the case that we have to bind
+                    
                     TextField("last Name:", text: $accountViewModel.user.lastName)
+                        .focused($focusedTextField, equals:.lastName)
+                        .onSubmit {
+                            focusedTextField = .email
+                        }
+                        .submitLabel(.next)
+                    
                     TextField("Email:", text: $accountViewModel.user.email)
+                        .focused($focusedTextField, equals:.email)
                         .keyboardType(.emailAddress)
                         .autocapitalization(.none)
                         .disableAutocorrection(true)
+                        .onSubmit {
+                            focusedTextField = nil
+                        }
+                        .submitLabel(.continue
+                        )
+                       // .focused($focusedTextField, equals:.email)
                     DatePicker("Birthday", selection: $accountViewModel.user.Birthdate, displayedComponents: .date)
                     Button {
                         accountViewModel.saveChanges()
