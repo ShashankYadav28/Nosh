9c5053caa31f7b0377996dac41b91949885b8488
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-09-20 10:09:50 +0530

    file: refractor

---
+++ Appetizer app/Utilities/Views/Display/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  Appetizer app
+//
+//  Created by Shashank Yadav on 20/09/25.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
