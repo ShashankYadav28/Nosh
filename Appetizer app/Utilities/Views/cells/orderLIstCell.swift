729a0bdd4180e2e90eef7707c4a8a46f43662234
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-09-25 14:41:52 +0530

    Appetizer list View nerw modifiers

---
+++ Appetizer app/Utilities/Views/cells/orderLIstCell.swift
@@ -0,0 +1,18 @@
+//
+//  orderLIstCell.swift
+//  Appetizer app
+//
+//  Created by Shashank Yadav on 25/09/25.
+//
+
+import SwiftUI
+
+struct orderLIstCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    orderLIstCell()
+}
