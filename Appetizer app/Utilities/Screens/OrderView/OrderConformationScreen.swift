73ac7080b18c17bf3200561d75e9f1d5a60af027
Shashank Yadav <ShashankYadav2803@gmail.com>
2025-09-27 15:51:21 +0530

    importing the combine module

---
+++ Appetizer app/Utilities/Screens/OrderView/OrderConformationScreen.swift
@@ -0,0 +1,18 @@
+//
+//  OrderConformationScreen.swift
+//  Appetizer app
+//
+//  Created by Shashank Yadav on 27/09/25.
+//
+
+import SwiftUI
+
+struct OrderConformationScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderConformationScreen()
+}
